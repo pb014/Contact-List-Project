9b74d1ebafa9c660737eee649020c0dd0d5aa611
Prakhar Beohar <prakharbeohar81@gmail.com>
2024-07-07 12:59:05 +0530

    project done

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+    return (
+        <p id="zero-state">
+            This is a demo for React Router.
+            <br />
+            Check out the docs at{" "}
+            <a href="https://reactrouter.com">
+                 reactrouter.com
+            </a>
+            .
+        </p>
+    );
+}
