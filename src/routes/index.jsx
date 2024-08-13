7715f2ab1575cc926f069cc3da2fce1c320378bf
Prakhar Beohar <prakharbeohar81@gmail.com>
2024-08-13 19:11:12 +0530

    changes done

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,3 @@
 export default function Index() {
-    return (
-        <p id="zero-state">
-            This is a demo for React Router.
-            <br />
-            Check out the docs at{" "}
-            <a href="https://reactrouter.com">
-                 reactrouter.com
-            </a>
-            .
-        </p>
-    );
+    
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -33,7 +33,7 @@ export default function Root() {
     return (
         <>
             <div id="sidebar">
-                <h1>React Router Contacts</h1>
+                <h1>Contacts</h1>
                 <div>
                     <Form id="search-form" role="search">
                         <input
